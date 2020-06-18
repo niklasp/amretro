7d95665dbb5e91d1e654b387bcacbb67b3945776
Niklas Jurij Plessing <contact@niklasplessing.net>
2020-06-18 11:56:24 +0200

    remove verbosity

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ paths.forEach( function( path ) {
 
   if ( el ) {
     path.addEventListener( 'mouseover', function( e ) {
-      console.log( id );
       el.style.display = 'block';
     } );
     path.addEventListener( 'mouseout', function( e ) {
@@ -19,7 +18,6 @@ paths.forEach( function( path ) {
 
   if ( ship ) {
     path.addEventListener( 'mouseover', function( e ) {
-      console.log( 'ship', id );
       ship.style.display = 'block';
     } );
     path.addEventListener( 'mouseout', function( e ) {
@@ -29,7 +27,6 @@ paths.forEach( function( path ) {
 } );
 
 imprintlink.addEventListener( 'click', function( e ) {
-  console.log( '><' );
   if ( imprint.style.display === 'block' ) {
     imprint.style.display = 'none';
   } else {
@@ -39,12 +36,11 @@ imprintlink.addEventListener( 'click', function( e ) {
 } );
 
 function resize() {
-  console.log( 'resize' );
   const bgImg = document.querySelector('.bg-image');
   const { width, height } = bgImg.getBoundingClientRect();
-  console.log( width, height );
   const svg = document.querySelector('.svg-wrap svg');
   const content = document.querySelector('.content');
+
   svg.setAttribute( 'width', `${ width }px` );
   svg.setAttribute( 'height', `${ height }px` );
   content.style.width = `${ width }px`;
